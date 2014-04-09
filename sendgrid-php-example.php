5749435d595ac02fbba9553bdbd8af79da30310b
root <root@9d623e9fe947.(none)>
2014-04-09 14:36:50 +0000

    Change sample code

--- sendgrid-php-example.php
+++ sendgrid-php-example.php
@@ -11,13 +11,18 @@ $sendgrid = new SendGrid($sendgrid_username, $sendgrid_password, array("turn_off
 $email    = new SendGrid\Email();
 $email->setTos($tos)->
        setFrom($from)->
-       setSubject('[sendgrid-php-example] フクロウのお名前は %fullname% さん')->
-       setText('%familyname% さんは何をしていますか？')->
-       setHtml('<strong> %familyname% さんは何をしていますか？</strong>')->
+       setFromName('送信者名')->
+       addBcc('awwa500@docomo.ne.jp')->
+       setSubject('[sendgrid-php-example] フクロウのお名前は%fullname%さん')->
+       setText('%familyname% さんは何をしていますか？\r\n 彼は%place%にいます。')->
+       setHtml('<strong> %familyname% さんは何をしていますか？</strong><br />彼は%place%にいます。')->
        addSubstitution("%fullname%", array("田中 太郎", "佐藤 次郎", "鈴木 三郎"))->
        addSubstitution("%familyname%", array("田中", "佐藤", "鈴木"))->
+       addSubstitution("%place%", array("%office%", "%home%", "%office%"))->
+       addSection('%office%', '中野')->
+       addSection('%home%', '目黒')->
+       addCategory('カテゴリ1')->
        addHeader('X-Sent-Using', 'SendGrid-API')->
-       addHeader('X-Transport', 'web')->
        addAttachment('./gif.gif', 'owl.gif');
 
 $response = $sendgrid->send($email);
