e79bf6129f2ac376414e4c8caf9c9001851ef79f
root <root@36b0085bf7fd.(none)>
2014-04-09 05:05:33 +0000

    Change example for Japanese

--- sendgrid-php-example.php
+++ sendgrid-php-example.php
@@ -4,17 +4,18 @@ Dotenv::load(__DIR__);
 
 $sendgrid_username = $_ENV['SENDGRID_USERNAME'];
 $sendgrid_password = $_ENV['SENDGRID_PASSWORD'];
-$to                = $_ENV['TO'];
+$from              = $_ENV['FROM'];
+$tos               = explode(',', $_ENV['TOS']);
 
 $sendgrid = new SendGrid($sendgrid_username, $sendgrid_password, array("turn_off_ssl_verification" => true));
 $email    = new SendGrid\Email();
-$email->addTo($to)->
-       setFrom($to)->
-       setSubject('[sendgrid-php-example] Owl named %yourname%')->
-       setText('Owl are you doing?')->
-       setHtml('<strong>%how% are you doing?</strong>')->
-       addSubstitution("%yourname%", array("Mr. Owl"))->
-       addSubstitution("%how%", array("Owl"))->
+$email->setTos($tos)->
+       setFrom($from)->
+       setSubject('[sendgrid-php-example] フクロウのお名前は %fullname% さん')->
+       setText('%familyname% さんは何をしていますか？')->
+       setHtml('<strong> %familyname% さんは何をしていますか？</strong>')->
+       addSubstitution("%fullname%", array("田中 太郎", "佐藤 次郎", "鈴木 三郎"))->
+       addSubstitution("%familyname%", array("田中", "佐藤", "鈴木"))->
        addHeader('X-Sent-Using', 'SendGrid-API')->
        addHeader('X-Transport', 'web')->
        addAttachment('./gif.gif', 'owl.gif');
